13ce20778c81c237e09b45c87861ce754f2a6781
Guang <jouyang19@proton.me>
2024-04-30 09:32:03 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  return Math.abs(distance - 42);
+}
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264;
+}
+function distanceFromHqInFeet(distance) {
+  return Math.abs(distance - 42) * 264;
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  let fare = 0;
+  if (distance <= 400) {
+    return fare;
+  } else if (distance < 2000 && distance > 400) {
+    fare = (distance - 400) * 0.02;
+    return fare;
+  } else if (distance > 2000 && distance < 2500) {
+    fare = 25;
+    return fare;
+  } else {
+    return "cannot travel that far";
+  }
+}
